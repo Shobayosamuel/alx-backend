0440d9dc61a4225d3ea980d0d7f9644953ce87f6
Samuel <shobayosamuel62@gmail.com>
2024-01-05 15:23:32 +0000

    redis asunc get value

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+import { promisify } from 'utils'
+
+const client = createClient();
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
